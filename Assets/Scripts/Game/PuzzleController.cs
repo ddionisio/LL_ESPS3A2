7a15f1758a0ef718f2b71cbd843c67b01a2d9647
David Dionisio <ddionisio@renegadeware.com>
2024-05-27 18:37:30 -0400

    some more setup, planning phase

---
+++ Assets/Scripts/Game/PuzzleController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LoLExt;
+
+/// <summary>
+/// General level game controller, use this as a basis for other levels with dialogs and learning stuff
+/// </summary>
+public class PuzzleController : GameModeController<PuzzleController> {
+
+	protected override void OnInstanceDeinit() {
+		//
+
+		base.OnInstanceDeinit();
+	}
+
+	protected override void OnInstanceInit() {
+		base.OnInstanceInit();
+
+		//
+	}
+
+	protected override IEnumerator Start() {
+		yield return base.Start();
+
+		//signal for puzzle to be playable
+
+		//wait for all goals to be finished
+
+		//jingle and pop-off
+
+		//move to marching band
+	}
+}
