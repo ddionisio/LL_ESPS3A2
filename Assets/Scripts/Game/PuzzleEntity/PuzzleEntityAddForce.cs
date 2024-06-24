4fe36118045a387b3a6f3eb436883601055e4a35
David Dionisio <ddionisio@renegadeware.com>
2024-06-24 17:06:27 -0400

    implement PuzzleEntityRigidbody and PuzzleEntityAddForce

--- Assets/Scripts/Game/PuzzleEntity/PuzzleEntityAddForce.cs
+++ Assets/Scripts/Game/PuzzleEntity/PuzzleEntityAddForce.cs
@@ -2,17 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleEntityAddForce : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PuzzleEntityAddForce : MonoBehaviour, IPuzzleEntityStateBegin, IPuzzleEntityStateUpdate {
+	public enum State {
+		None,
+		Impulse,
+		Force
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Rigidbody2D body;
+
+	public float dirAngle;
+
+	public float impulse;
+	public float force;
+	public float duration;	
+
+	public PuzzleEntityState state;
+
+	private State mForceState;
+
+	private float mForceCurTime;
+
+	private Vector2 mDir;
+
+	void IPuzzleEntityStateBegin.OnStateBegin(PuzzleEntityState state) {
+		if(state == this.state) {
+			if(impulse != 0f)
+				mForceState = State.Impulse;
+			else if(force != 0f)
+				mForceState = State.Force;
+
+			mForceCurTime = 0f;
+
+			mDir = M8.MathUtil.RotateAngle(Vector2.up, dirAngle);
+		}
+		else
+			mForceState = State.None;
+	}
+
+	bool IPuzzleEntityStateUpdate.OnStateUpdate(PuzzleEntityState state) {
+		var ret = false;
+
+		switch(mForceState) {
+			case State.Impulse:
+				body.AddForce(mDir * impulse, ForceMode2D.Impulse);
+
+				if(force != 0f)
+					mForceState = State.Force;
+				break;
+
+			case State.Force:
+				body.AddForce(mDir * force, ForceMode2D.Force);
+
+				mForceCurTime += Time.deltaTime;
+				ret = mForceCurTime >= duration;
+				break;
+
+			default:
+				ret = true;
+				break;
+		}
+
+		return ret;
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+
+		var dir = M8.MathUtil.RotateAngle(Vector2.up, dirAngle);
+		M8.Gizmo.Arrow(transform.position, dir);
+	}
 }
--- Assets/Scripts/Game/PuzzleEntity/PuzzleEntityRigidbody.cs
+++ Assets/Scripts/Game/PuzzleEntity/PuzzleEntityRigidbody.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleEntityRigidbody : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PuzzleEntityRigidbody : MonoBehaviour, IPuzzleEntityStateBegin {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+	public Rigidbody2D body;
+
+	public bool applySimulated;
+    public PuzzleEntityState[] simulatedStates;
+
+    public bool applyKinematic;
+	public PuzzleEntityState[] kinematicStates;	
+
+    void IPuzzleEntityStateBegin.OnStateBegin(PuzzleEntityState state) {
+        if(body) {
+            if(applySimulated) {
+                var isSimulated = false;
+                for(int i = 0; i < simulatedStates.Length; i++) {
+                    if(simulatedStates[i] == state) {
+                        isSimulated = true;
+                        break;
+                    }
+                }
+
+                body.simulated = isSimulated;
+            }
+
+            if(applyKinematic) {
+                var isKinematic = false;
+                for(int i = 0; i < kinematicStates.Length; i++) {
+                    if(kinematicStates[i] == state) {
+                        isKinematic = true;
+                        break;
+                    }
+                }
+
+                body.isKinematic = isKinematic;
+            }
+        }
     }
 }
