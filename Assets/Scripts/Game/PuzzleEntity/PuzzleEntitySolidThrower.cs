c49b5baedc54ba7fa9930679456e2f89f5170149
David Dionisio <ddionisio@renegadeware.com>
2024-09-23 17:11:23 -0400

    more level 5 prototyping

--- Assets/Scripts/Game/PuzzleEntity/PuzzleEntitySolidThrower.cs
+++ Assets/Scripts/Game/PuzzleEntity/PuzzleEntitySolidThrower.cs
@@ -6,23 +6,34 @@ public class PuzzleEntitySolidThrower : MonoBehaviour {
 	
 	public GameObject solidRootGO;
 
+	[Header("Animation")]
+	public M8.AnimatorParamTrigger animEnterTrigger;
+	public M8.AnimatorParamTrigger animActionTrigger;
+
 	public bool isBusy { get { return mRout != null; } }
 
 	private M8.CacheList<PuzzleEntitySolid> mSolidActives;
 	private M8.CacheList<PuzzleEntitySolid> mSolidIdles;
 
+	private Animator mAnim;
+
 	private Coroutine mRout;
 
 	public void Enter() {
-		
-	}
+		StopRout();
 
-	public void Exit() {
-
+		mRout = StartCoroutine(DoEnter());
 	}
 
 	public void Action() {
+		if(isBusy)
+			return;
 
+		mRout = StartCoroutine(DoAction());
+	}
+
+	void OnDisable() {
+		StopRout();
 	}
 
 	void OnDestroy() {
@@ -43,6 +54,8 @@ public class PuzzleEntitySolidThrower : MonoBehaviour {
 
 			mSolidIdles.Add(solid);
 		}
+
+		mAnim = GetComponent<Animator>();
 	}
 
 	void OnSolidSpawned(PuzzleEntitySolid solid) {
@@ -58,29 +71,50 @@ public class PuzzleEntitySolidThrower : MonoBehaviour {
 
 	IEnumerator DoEnter() {
 		//animation enter
+		if(mAnim) {
+			animEnterTrigger.Set(mAnim);
 
-		//show each solid one by one
+			yield return M8.AnimatorUtil.WaitNextState(mAnim);
+		}
+		else
+			yield return null;
 
-		//done
-		yield return null;
-
-		mRout = null;
-	}
-
-	IEnumerator DoExit() {
-		//respawn all solids
-
-		//animation exit
-
-		//done
-		yield return null;
+		//show solids
+		for(int i = 0; i < mSolidIdles.Count; i++) {
+			var solid = mSolidIdles[i];
+			if(solid)
+				solid.active = true;
+		}
 
 		mRout = null;
 	}
 
 	IEnumerator DoAction() {
-		yield return null;
+		//animation act
+		if(mAnim) {
+			animActionTrigger.Set(mAnim);
+
+			yield return M8.AnimatorUtil.WaitNextState(mAnim);
+		}
+		else
+			yield return null;
+
+		//launch an idle solid
+		if(mSolidIdles.Count > 0) {
+			var solid = mSolidIdles.Remove();
+
+			solid.Action(1f);
+
+			mSolidActives.Add(solid);
+		}
 
 		mRout = null;
+	}
+
+	private void StopRout() {
+		if(mRout != null) {
+			StopCoroutine(mRout);
+			mRout = null;
+		}
 	}
 }
--- Assets/Scripts/Game/PuzzleEntity/PuzzleEntitySpiritFire.cs
+++ Assets/Scripts/Game/PuzzleEntity/PuzzleEntitySpiritFire.cs
@@ -3,5 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleEntitySpiritFire : MonoBehaviour {
-    
+
+	[Header("Power")]
+	public M8.RangeFloat powerRange;
+
+	[Header("Fuel")]
+	public float fuelAmount; //per solids
+	public M8.RangeFloat fuelBurnRateRange; //determined by wind scalar value [0, 1]
+
+	[Header("Wind Source")]
+	public float windCapacity;
+	public float windRate; //when accumulating wind
+	public float windDecayWaitDelay;
+	public float windDecayRate;
+
+	public float power { get; private set; }
+
+	public float wind { get; private set; }
+
+	public float fuel { get; private set; }
+
+	private const int solidFuelCapacity = 8;
+	private M8.CacheList<PuzzleEntitySolid> mSolidFuels = new M8.CacheList<PuzzleEntitySolid>(solidFuelCapacity);
+
+	private Coroutine mWindRout;
+	private Coroutine mFuelRout;
+
+	public void WindBlow() {
+		if(mWindRout != null)
+			StopCoroutine(mWindRout);
+
+		mWindRout = StartCoroutine(DoWind());
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		
+	}
+
+	void OnEnable() {
+		power = powerRange.min;
+		wind = 0f;
+		fuel = 0f;
+	}
+
+	void OnDisable() {
+		if(mWindRout != null) {
+			StopCoroutine(mWindRout);
+			mWindRout = null;
+		}
+
+		if(mFuelRout != null) {
+			StopCoroutine(mFuelRout);
+			mFuelRout = null;
+		}
+
+		//clear out solid fuels
+		for(int i = 0; i < mSolidFuels.Count; i++) {
+			var solid = mSolidFuels[i];
+			if(solid && solid.state == PuzzleEntityState.Action)
+				solid.Respawn();
+		}
+
+		mSolidFuels.Clear();
+	}
+
+	IEnumerator DoWind() {
+		yield return null;
+
+		mWindRout = null;
+	}
+
+	IEnumerator DoFuel() {
+		yield return null;
+
+		mFuelRout = null;
+	}
 }
